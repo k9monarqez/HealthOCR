59a857c66d4bb6403de34db9c3b499acb620b17f
k9monarqez <mlgmaker81@gmail.com>
2026-05-09 23:38:26 +0300

    update app name

--- app/src/main/java/com/example/healthocr/ui/theme/Theme.kt
+++ app/src/main/java/com/example/healthocr/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun SwagaAppTheme(
+fun HealthOCRTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
